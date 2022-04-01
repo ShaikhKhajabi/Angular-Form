4e437ef7445e8f86815a1bcbd8eb728ee62cfc91
Khajabi Shaikh <khajabi0410@gmail.com>
2022-04-01 07:46:06 +0530

    simple interface

---
+++ src/app/data/user-setting.ts
@@ -0,0 +1,9 @@
+
+export interface UserSettings {
+    name: string | null;
+    emailoffers: boolean | null;
+    interfaceStyle: string | null;
+    subscriptionType: string | null;
+    notes: string | null
+
+}
